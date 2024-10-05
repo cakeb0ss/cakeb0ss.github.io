e1783623c72dd277f6d3ca37633d842a993934c9
cakeb0ss <167144060+cakeb0ss@users.noreply.github.com>
2024-10-04 23:23:48 -0400

    added counter, template implementation

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
 const doc = document;
 const body = doc.body;
-const main = doc.getElementById('main');
+let main = doc.getElementById('main');
 const mainTemps = doc.getElementsByTagName('template');
 
-mainTemps.forEach(nameAll = () => {
-    let i = 0;
-    let elems = [];
-    if (i < mainTemps.length) {
+let contentDiv = doc.getElementById('content');
+
+
+mainDict = Object.entries(mainTemps);
+let elems = [];
+
+function getElems(elems) {
+    for (i=0; i < mainDict.length; i++) {
         eval(`
-        let elem${i} = mainTemps.querySelector('div');
+        let elem${i} = mainDict[${i}];
         elems.push(elem${i});
         `);
-        i++;
     }
-})
+    return elems;
+}
 
-doc.onload = pageLoad;
+getElems(elems);
+console.table(elems);
+
+let header, content, regCounter;
+header = elems[0][1].innerHTML;
+content = elems[1][1].innerHTML;
+regCounter = elems[2][1].innerHTML;
+
+
+
+for (let i=0;i < elems.length; i++) {
+    console.log(elems[i]);
+}
+
+console.log(header);
+console.log(content);
+console.log(regCounter);
+
+
 
 function pageLoad () {
-    let header, content;
-    header = doc.importNode(mainTemps[0]);
-    content = doc.importNode(mainTemps[1]);
 
-    main.appendChild(header);
-    main.appendChild(content);
+    main.innerHTML = '';
+    main.innerHTML += header;
+    main.innerHTML += content;
 
-    console.log(mainTemps);
+}
+
+doc.onload = pageLoad();
+
+
+function counter() {
+
+    console.log(regCounter);
+    main.innerHTML = '';
+    main.innerHTML += header;
+    main.innerHTML += regCounter;
+
+}
+
+function counterCompute() {
+    let p = doc.getElementById('pennies').value * 0.01;
+    let n = doc.getElementById('nickels').value * 0.05;
+    let d = doc.getElementById('dimes').value * 0.10;
+    let q = doc.getElementById('qs').value * 0.25;
+    let ones = doc.getElementById('ones').value * 1;
+    let fives = doc.getElementById('five').value * 5;
+    let tens = doc.getElementById('ten').value * 10;
+    let $_20s = doc.getElementById('twen').value * 20;
+    let $_50s = doc.getElementById('fift').value * 50;
+    let $_100s = doc.getElementById('hund').value * 100;
+    let PRs = doc.getElementById('prs').value * 0.50;
+    let NRs = doc.getElementById('nrs').value * 2;
+    let DRs = doc.getElementById('drs').value * 5;
+    let QRs = doc.getElementById('qrs').value * 10;
+    
+    let total = (p+n+d+q+ones+fives+tens+$_20s+$_50s+$_100s+PRs+NRs+DRs+QRs);
+    let dep = total - 400.00;
+
+    console.log(total, dep)
+    let totalDiv = doc.getElementById('total');
+    let depDiv = doc.getElementById('deposit');
+    totalDiv.innerHTML = `Total: $${total.toFixed(2)}`;
+    depDiv.innerHTML = `Deposit: $${dep.toFixed(2)}`;
+}
+
+function clearInputs() {
+
 }
